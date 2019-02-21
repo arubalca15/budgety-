c637b9ecf18a14366256a0696bae80a2dd127c2b
Amado Rubalcava <45728638+arubalca15@users.noreply.github.com>
2019-02-20 19:58:22 -0800

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ var budgetController = (function() {
     
     var totalExpenses = 0;
     
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum; // this will fill in the totals that are initially zero
+    };
+    
     var data = {
         allItems: {
             exp: [],
@@ -24,7 +32,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
     return {
         addItem: function(type, descript, val) {
@@ -46,8 +56,33 @@ var budgetController = (function() {
             
             data.allItems[type].push(newItem); //pushed it into our data structure
             return newItem; //returns the new element
+        },
+        
+         calculateBudget: function() {
+            
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // Calculate the budget: income - expenses
+             data.budget = data.totals.inc - data.totals.exp;
+             //Calculate percentage of income we make
+             if(data.totals.inc > 0) {
+                 
+                 data.percentage = Math.round((data.totals.exp/data.totals.inc) * 100); } else{
+                 data.percentage = -1;
+             }
+         },
+        
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
         }
-    }
+    };
 })();
 
 //UI CONTROLLER
@@ -58,9 +93,13 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__button',
+        inputBtn: '.add__btn',
         incomeContainer : '.income__list',
-        expensesContainer : '.expenses__list'
+        expensesContainer : '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
         
     };
     
@@ -69,7 +108,7 @@ var UIController = (function() {
             return{
                 type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp 
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value 
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)// parseFloat converts a string to a floating decimal 
             };   
         },
         
@@ -95,9 +134,33 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml); //(where do we want to insert it, what we want to insert)
         },
         
+        clearFields: function() {
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);//querySelectorAll does not return an array but a list. This means we do not get access to the method associated with arrays.
+            
+            fieldsArr = Array.prototype.slice.call(fields);// we have to use slice to make a copy of an array. We use the call method to get the list from var fields to make a copy in an array. THIS MAKES AN ARRAY 
+            
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";// this clears the fields
+            });//instead of using a forlopp we use the forEach method to pass a callback function through each index of the array 
+            
+            fieldsArr[0].focus(); // this puts the cursor back to description
+        },
+        
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+        },
         
         getDOMstrings : function() {
             return DOMstrings;
+        },
+        
+        testing: function() {
+            console.log(data);
         }
         
     };
@@ -120,6 +183,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         }); // keypress is a predetermined word that tracts when a key is pressed 
     };
     
+    var updateBudget = function() {
+        
+        // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
+        
+        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
+        // 3. Display the budget on the UI
+        UICtrl.displayBudget(budget);
+    }; // updates the bugdet everytime we input something
      
     
     var ctrlAddItem = function(){
@@ -128,15 +201,21 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 1. Get the field input data
         input = UICtrl.getinput();
         
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value); // we grab out input from the 1st step and create a new item 
-        
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type); 
-        // 4. Calculate the budget
-        
-        // 5. Display the budget on the UI 
-        
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {  //this checks if the input is a number,
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value); // we grab out input from the 1st step and create a new item 
+
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type); 
+
+            // 4. Clear the fields
+
+            UICtrl.clearFields();
+
+            // 5. Calculate the budget
+
+            // 6. Display the budget on the UI 
+        }
     }; // THIS IS THE FUNCTION THAT IS USED WHEN THE ENTER KEY IS PRESSED 
     
     
